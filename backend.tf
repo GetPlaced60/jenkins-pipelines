5a81ec991a46635786f40365281f0bc0a4580873
GetPlaced60 <getplaced60@gmail.com>
2024-06-20 15:30:21 +0530

    update

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backup-terra-state-files"
+    bucket = "terraform-state-bucket-ccw2"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamoDB-table"
